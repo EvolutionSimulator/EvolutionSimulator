e05dc05082b9b90aaaaf1f8615e94d0987d7fb81
berra-dogan <berra.dogan129@gmail.com>
2023-11-29 12:13:07 +0100

    Disable Neurons/Links ver-1
    It is the initial version and is not fully complete.

--- Engine/include/neat/neat-neuron.h
+++ Engine/include/neat/neat-neuron.h
@@ -1,6 +1,10 @@
 #ifndef NEATNEURON_H
 #define NEATNEURON_H
 
+#include "neat-link.h"
+
+#include <vector>
+
 namespace neat {
 
 enum class NeuronType { kInput, kOutput, kHidden };
@@ -12,11 +16,15 @@ class Neuron {
   int GetId() const;
   NeuronType GetType() const;
   double GetBias() const;
+  bool IsActive() const;
 
   void SetBias(double bias);
+  void SetActive();
+  void SetInactive();
 
  private:
   int id_;
+  bool active_ = true;
   NeuronType type_;
   double bias_;
 };
