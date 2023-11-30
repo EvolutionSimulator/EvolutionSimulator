79d977386745fc2f1cb45dc95b7807808fb1cf5d
Adrien <77799977+spieredd@users.noreply.github.com>
2023-11-30 09:10:32 +0100

    created a render food

--- UI/src/simulationcanvas.cpp
+++ UI/src/simulationcanvas.cpp
@@ -32,12 +32,24 @@ void SimulationCanvas::OnUpdate() {
 // use this to process the simulation data and render it on the screen
 void SimulationCanvas::RenderSimulation(SimulationData* data)
 {
-    clear(sf::Color(0, 255, 0));
+    clear(sf::Color(9, 109, 6));
 
-    // Iterate through entities and create a circle shape for each
+    // Iterate through food and create a circle shape for each
+    for (const auto& food : data->food_entities_) {
+        sf::CircleShape foodShape(5);
+        sf::Color myColor(100, 200, 98);
+        foodShape.setFillColor(myColor);
+
+        std::pair<double, double> foodCoordinates = food.GetCoordinates();
+        foodShape.setPosition(foodCoordinates.first, foodCoordinates.second);
+        draw(foodShape);
+    }
+
+    // Iterate through creatures and create a circle shape for each
     for (const auto& creature : data->creatures_) {
         sf::CircleShape shape(10);
-
+        sf::Color myColor(89, 109, 30);
+        shape.setFillColor(myColor);
         std::pair<double, double> coordinates = creature.GetCoordinates();
         shape.setPosition(coordinates.first, coordinates.second);
 
