10db12a5b4efa7774b6753c76357d519b529712e
GianlucaOberreit <gianluca.oberreit@polytechnique.edu>
2023-11-30 13:56:26 +0100

    Bound rotation to [-pi,pi] and fix bugs

--- Engine/src/movable_entity.cpp
+++ Engine/src/movable_entity.cpp
@@ -23,15 +23,16 @@ void MovableEntity::SetRotationalVelocity(double rotational_velocity) {
 
 void MovableEntity::Rotate(double deltaTime)
 {
-    this->SetOrientation(this->GetOrientation() + deltaTime * this->GetRotationalVelocity());
+    double rotational_velocity = fmod(this->GetOrientation() + deltaTime * this->GetRotationalVelocity(), PI);
+    this->SetOrientation(rotational_velocity);
 }
 
 void MovableEntity::Move(double deltaTime)
 {
     std::pair<double, double> coordinates = this->GetCoordinates();
     double orientation = this->GetOrientation();
-    double x_velocity = this->GetVelocityForward() * sin(orientation);
-    double y_velocity = this->GetVelocityForward() * cos(orientation);
+    double x_velocity = this->GetVelocityForward() * cos(orientation);
+    double y_velocity = this->GetVelocityForward() * sin(orientation);
     coordinates.first = coordinates.first + deltaTime * x_velocity;
     coordinates.second = coordinates.second + deltaTime * y_velocity;
     this->SetCoordinates(coordinates.first, coordinates.second);
