3df925c904da253ce599fb2f806adb24cfd0d962
Charlielv04 <Carloslv04@gmail.com>
2023-12-01 14:07:16 +0100

    Change import from stdlib.h to cstdlib

--- Engine/src/food.cpp
+++ Engine/src/food.cpp
@@ -1,21 +1,13 @@
 #include "food.h"
-<<<<<<< HEAD
 #include "cstdlib"
 #include "environment.h"
 Food::Food()
     : Entity(std::rand()%Environment::kMaxFoodSize)
-=======
-#include <stdlib.h>
-#include "environment.h"
-Food::Food()
-    : Entity()
->>>>>>> 0e157bd (Nutritional value to size)
 {
     // Constructor implementation
 }
 
 Food::Food(const double x_coord, const double y_coord)
-<<<<<<< HEAD
     : Entity(x_coord, y_coord, std::rand()%Environment::kMaxFoodSize)
 {
     //Constructor implementation
@@ -24,12 +16,7 @@ Food::Food(const double x_coord, const double y_coord)
 Food::Food(const double size)
     : Entity(size)
 {
-
-=======
-    : Entity(x_coord, y_coord, rand()%Environment::kMaxFoodSize)
-{
     //Constructor implementation
->>>>>>> 0e157bd (Nutritional value to size)
 }
 
 void Food::Eat()
