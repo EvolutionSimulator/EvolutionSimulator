9f6adc7a4df82eabc93f890e1ae2b24457c26b0d
Charlielv04 <Carloslv04@gmail.com>
2023-12-01 14:32:42 +0100

    Add energy management
    Implement energy property for creatures, die, and update functions

--- Engine/include/creature.h
+++ Engine/include/creature.h
@@ -5,13 +5,24 @@
 
 class Creature : public MovableEntity {
 public:
+    enum states {
+        Dead = 0,
+        Alive = 1
+    };
     Creature();
 
+    void Dies();
+    void Eats(double nutritional_value);
+    void UpdateEnergy();
     double GetEnergy() const;
     void SetEnergy(double energy);
 
+    states GetState() const;
+    void SetState (states state);
+
 private:
     double energy_;
+    states state_;
 };
 
 #endif // CREATURE_HPP
--- Engine/src/creature.cpp
+++ Engine/src/creature.cpp
@@ -14,3 +14,27 @@ double Creature::GetEnergy() const {
 void Creature::SetEnergy(double energy) {
     energy_ = energy;
 }
+
+void Creature::UpdateEnergy(){
+    SetEnergy( GetEnergy() - (GetVelocityForward() + GetRotationalVelocity()) * GetSize());
+
+    if (GetEnergy() <= 0){
+        Dies();
+    }
+}
+
+void Creature::Dies(){
+    SetState(Dead);
+}
+
+void Creature::Eats(double nutritional_value){
+    SetEnergy(GetEnergy() + nutritional_value);
+}
+
+Creature::states Creature::GetState() const{
+    return state_;
+}
+
+void Creature::SetState(states state){
+    state_ = state;
+}
