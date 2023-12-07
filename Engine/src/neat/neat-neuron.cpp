0444e9ad2f0c0561f33d062259db27bffe4ebf5f
aya-matmata <147956677+aya-matmata@users.noreply.github.com>
2023-12-07 01:18:02 +0100

    Implementing crossover (#23)
    * Implementing crossover
    
    (using neurons and links crossovers)
    
    * Implementing crossover
    
    (using neurons and links crossovers)
    
    * tests for crossovers
    
    ---------
    
    Co-authored-by: Huy Le <12729642+atoiz@users.noreply.github.com>

--- Engine/src/neat/neat-neuron.cpp
+++ Engine/src/neat/neat-neuron.cpp
@@ -1,5 +1,6 @@
 #include "neat/neat-neuron.h"
-
+#include "assert.h"
+#include "random"
 namespace neat {
 
 Neuron::Neuron(int id, NeuronType type, double bias)
@@ -19,4 +20,18 @@ void Neuron::SetActive(){active_= true;}
 
 void Neuron::SetInactive(){active_ = false;}
 
+Neuron CrossoverNeuron(const Neuron &a, const Neuron &b){
+    assert(a.GetId() == b.GetId());
+    assert(a.GetType() == b.GetType());
+    int id = a.GetId();
+    NeuronType type = a.GetType();
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<double> distribution(0.0, 1.0);
+
+    double bias = distribution(gen) < 0.5 ? a.GetBias() : b.GetBias();
+
+    return {id, type, bias};
+}
 }  // namespace neat
