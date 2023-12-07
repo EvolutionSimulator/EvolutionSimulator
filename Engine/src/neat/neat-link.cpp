a3dc8db84fc6d5a0a0410786e5b6715fd715c4f1
Huy Le <12729642+atoiz@users.noreply.github.com>
2023-12-07 04:59:03 +0100

    Reformat code

--- Engine/src/neat/neat-link.cpp
+++ Engine/src/neat/neat-link.cpp
@@ -1,4 +1,5 @@
 #include "neat/neat-link.h"
+
 #include "random"
 
 namespace neat {
@@ -6,7 +7,11 @@ namespace neat {
 int Link::next_id_ = 1;
 
 Link::Link(int in_id, int out_id, double weight)
-    : id_(next_id_++), in_id_(in_id), out_id_(out_id), weight_(weight), active_(true) {}
+    : id_(next_id_++),
+      in_id_(in_id),
+      out_id_(out_id),
+      weight_(weight),
+      active_(true) {}
 
 int Link::GetId() const { return id_; }
 
@@ -20,26 +25,27 @@ bool Link::IsActive() const { return active_; }
 
 void Link::SetWeight(double weight) { weight_ = weight; }
 
-void Link::SetActive(){active_= true;}
+void Link::SetActive() { active_ = true; }
 
-void Link::SetInactive(){active_ = false;}
+void Link::SetInactive() { active_ = false; }
 
 Link NewLink(int in_id, int out_id, double weight);
 
-Link CrossoverLink(const Link &a, const Link &b){
-    if (a.GetId() != b.GetId()) {
-        throw std::invalid_argument("Links must have the same id");
-    }
+Link CrossoverLink(const Link &a, const Link &b) {
+  if (a.GetId() != b.GetId()) {
+    throw std::invalid_argument("Links must have the same id");
+  }
 
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_real_distribution<double> distribution(0.0, 1.0);
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<double> distribution(0.0, 1.0);
 
-    Link crossover_link = a;
+  Link crossover_link = a;
 
-    crossover_link.SetWeight(distribution(gen) < 0.5 ? a.GetWeight() : b.GetWeight());
+  crossover_link.SetWeight(distribution(gen) < 0.5 ? a.GetWeight()
+                                                   : b.GetWeight());
 
-    return crossover_link;
+  return crossover_link;
 }
 
 }  // namespace neat
