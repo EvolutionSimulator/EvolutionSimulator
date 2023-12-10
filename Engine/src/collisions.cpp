1037af416f442beac77ca1c57a9b8183f3562c75
berra-dogan <berra.dogan129@gmail.com>
2023-12-10 11:38:46 +0100

    Circle-Line Collision Helper Function Fix

--- Engine/src/collisions.cpp
+++ Engine/src/collisions.cpp
@@ -7,31 +7,27 @@ double EuclideanDistance(const std::pair<double, double>& point1, const std::pai
 
 double ShortestDistancePointLineSegment(const std::pair<double, double>& point, const std::pair<double, double>& linePoint1, const std::pair<double, double>& linePoint2) {
     // Calculate coefficients of the line equation Ax + By + C = 0
-    double A = linePoint2.second - linePoint1.second;
-    double B = linePoint1.first - linePoint2.first;
-    double C = linePoint1.first * linePoint2.second - linePoint2.first * linePoint1.second;
-
-    // Calculate the distance using the formula for an infinite line
-    double dist = std::abs(A * point.first + B * point.second + C) / std::sqrt(A * A + B * B);
-
-    // Calculate the squared length of the line segment
-    double lineSegmentLengthSquared = EuclideanDistance(linePoint1, linePoint2) * EuclideanDistance(linePoint1, linePoint2);
-
-    // Calculate the dot product of vectors (point - linePoint1) and (linePoint2 - linePoint1)
-    double dotProduct = ((point.first - linePoint1.first) * (linePoint2.first - linePoint1.first)) +
-                        ((point.second - linePoint1.second) * (linePoint2.second - linePoint1.second));
-
-    // Calculate the parameter t (distance from linePoint1 to the closest point on the line)
-    double t = dotProduct / lineSegmentLengthSquared;
-
-    // Check if the closest point falls within the line segment
-    if (t < 0.0) { // Closest point is beyond linePoint1
-        dist = EuclideanDistance(point, linePoint1);
-    } else if (t > 1.0) { // Closest point is beyond linePoint2
-        dist = EuclideanDistance(point, linePoint2);
+    double a, b, c;
+    if (linePoint2.first - linePoint1.first == 0){
+        a = 1.0;
+        b = 0.0;
+        c = -linePoint1.first;
+    }
+    else{
+        a = -(linePoint2.second - linePoint1.second)/(linePoint2.first - linePoint1.first);
+        b = 1.0;
+        c = linePoint1.second + a * linePoint1.first;
     }
 
-    return dist;
+    const double distance = std::abs(a * point.first + b * point.second + c) / std::sqrt(std::pow(a, 2) + std::pow(b, 2));
+    double x = (b * (b * point.first - a * point.second) - a * c) / (std::pow(a, 2) + std::pow(b, 2));
+    //double y = (-a * (b * point.first - a * point.second) - b * c) / (std::pow(a, 2) + std::pow(b, 2));
+
+    if (x <= std::max(linePoint1.first, linePoint2.first) && x >= std::min(linePoint1.first, linePoint2.first)){
+        return distance;
+    }
+
+    return std::min(EuclideanDistance(point, linePoint1), EuclideanDistance(point, linePoint2));
 }
 
 bool CollisionCircleCircle(const double& tolerance, const std::pair<double, double>& center1, const double& radius1, const std::pair<double, double>& center2, const double& radius2){
