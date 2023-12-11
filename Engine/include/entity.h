2b79d144e6c8919289ee12a5df8148baf0a7b11e
GianlucaOberreit <gianluca.oberreit@polytechnique.edu>
2023-12-11 13:51:16 +0100

    Add creature growth
    Add method for creature growth and attribute for growth to energy
    consumption ration.

--- Engine/include/entity.h
+++ Engine/include/entity.h
@@ -1,35 +1,34 @@
 #ifndef ENTITY_H
 #define ENTITY_H
 
-#include <vector>
 #include "collisions.h"
+#include <vector>
 
-class Entity
-{
+class Entity {
 public:
-    enum states {
-        Dead = 0,
-        Alive = 1
-    };
-    Entity();
-    Entity(const double x_coord, const double y_coord, const double size);
-    Entity(const double size);
-    virtual ~Entity();
+  enum states { Dead = 0, Alive = 1 };
+  Entity();
+  Entity(const double x_coord, const double y_coord, const double size);
+  Entity(const double size);
+  virtual ~Entity();
 
-    void RandomInitialization(const double world_width, const double world_height, const double max_creature_size);
+  void RandomInitialization(const double world_width, const double world_height,
+                            const double max_creature_size);
 
-    double GetSize() const;
-    void SetSize(double size);
+  double GetSize() const;
+  void SetSize(double size);
 
-    std::pair<double, double> GetCoordinates() const;
-    void SetCoordinates(const double x, const double y, const double kMapWidth, const double kMapHeight);
+  std::pair<double, double> GetCoordinates() const;
+  void SetCoordinates(const double x, const double y, const double kMapWidth,
+                      const double kMapHeight);
 
-    double GetOrientation() const;
-    void SetOrientation(double orientation);
+  double GetOrientation() const;
+  void SetOrientation(double orientation);
 
-    states GetState() const;
-    void SetState (states state);
+  states GetState() const;
+  void SetState(states state);
 
+<<<<<<< HEAD
     virtual void OnCollision(Entity& other_entity);
 
     double GetDistance(const Entity& otherEntity) const;
@@ -40,6 +39,19 @@ public:
 protected:
     double x_coord_, y_coord_, orientation_, size_;
     states state_;
+=======
+  double GetDistance(const Entity &otherEntity) const;
+  bool CheckCollisionWithEntity(const double tolerance,
+                                const Entity &otherEntity) const {
+    return CollisionCircleCircle(tolerance, GetCoordinates(), GetSize(),
+                                 otherEntity.GetCoordinates(),
+                                 otherEntity.GetSize());
+  }
+
+private:
+  double x_coord_, y_coord_, orientation_, size_;
+  states state_;
+>>>>>>> a1a2713 (Add creature growth)
 };
 
 #endif // ENTITY_H
