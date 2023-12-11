a1a2713d85867667e916f767d6ea2e758735ba2c
GianlucaOberreit <gianluca.oberreit@polytechnique.edu>
2023-12-11 13:51:16 +0100

    Add creature growth
    Add method for creature growth and attribute for growth to energy
    consumption ration.

--- Engine/include/creature.h
+++ Engine/include/creature.h
@@ -1,42 +1,53 @@
 #ifndef CREATURE_HPP
 #define CREATURE_HPP
 
-#include "movable_entity.h"
-#include "food.h"
-#include "neat/neat-neural-network.h"
 #include "config.h"
+#include "food.h"
+#include "movable_entity.h"
+#include "neat/neat-neural-network.h"
 
 #include <unordered_map>
 
 class Creature : public MovableEntity {
 public:
+  Creature(neat::Genome genome);
 
-    Creature(neat::Genome genome);
+  void Dies();
+  void Eats(double nutritional_value);
+  void UpdateEnergy();
+  double GetEnergy() const;
+  void SetEnergy(double energy);
+  neat::Genome GetGenome();
+  void Update(double deltaTime, double const kMapWidth,
+              double const kMapHeight);
 
-    void Dies();
-    void Eats(double nutritional_value);
-    void UpdateEnergy();
-    double GetEnergy() const;
-    void SetEnergy(double energy);
-    neat::Genome GetGenome();
-    void Update(double deltaTime, double const kMapWidth, double const kMapHeight);
+  virtual void OnCollision(Food &food);
+  virtual void OnCollision(Creature &creature);
+  Food *GetClosestFood(
+      std::unordered_map<int, std::unordered_map<int, std::vector<Entity *>>>
+          &grid,
+      double GridCellSize) const;
 
-    virtual void OnCollision(Food& food);
-    virtual void OnCollision(Creature& creature);
-    Food* GetClosestFood
-        (std::unordered_map<int, std::unordered_map<int, std::vector<Entity*> > > &grid,
-         double GridCellSize) const;
+  void SetGrowthFactor(double growth_factor);
+  double GetGrowthFactor();
 
-    bool Fit();
+  void SetMaxSize(double max_size);
+  double GetMaxSize();
+
+  void Grow(double energy);
+
+  bool Fit();
 
 private:
-    double energy_;
-    neat::NeuralNetwork brain_;
-    neat::Genome genome_;
+  double energy_, growth_factor_;
+  double max_size_ = 10;
+  neat::NeuralNetwork brain_;
+  neat::Genome genome_;
 };
 
-std::vector<Food*> get_food_at_distance
-    (std::unordered_map<int, std::unordered_map<int, std::vector<Entity*> > > &grid,
-     int i_creature, int j_creature, int grid_distance);
+std::vector<Food *> get_food_at_distance(
+    std::unordered_map<int, std::unordered_map<int, std::vector<Entity *>>>
+        &grid,
+    int i_creature, int j_creature, int grid_distance);
 
 #endif // CREATURE_HPP
