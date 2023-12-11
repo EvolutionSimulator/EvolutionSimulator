e128b73dc300b92915f236e3bfbab946868d92c9
Charlielv04 <Carloslv04@gmail.com>
2023-12-11 12:45:24 +0100

    Add brain processing

--- Engine/src/creature.cpp
+++ Engine/src/creature.cpp
@@ -72,9 +72,12 @@ void Creature::Eats(double nutritional_value){
 }
 
 void Creature::Update(double deltaTime, double const kMapWidth,
-                      double const kMapHeight) {
+                      double const kMapHeight,
+                      std::unordered_map<int, std::unordered_map<int, std::vector<Entity*> > > &grid,
+                      double GridCellSize) {
   this->Move(deltaTime, kMapWidth, kMapHeight);
   this->Rotate(deltaTime);
+  this->Think(grid, GridCellSize);
 }
 
 neat::Genome Creature::GetGenome() { return genome_; }
@@ -89,7 +92,6 @@ void Creature::OnCollision(Entity& other_entity)
             food->Eat();
         }
     }
-
     else
     {
         Entity::OnCollision(other_entity);
@@ -102,15 +104,28 @@ void Creature::SetGrowthFactor(double growth_factor) {
 
 double Creature::GetGrowthFactor() { return growth_factor_; }
 
-void Creature::Think(std::unordered_map<int, std::unordered_map<int, std::vector<Entity*> > > &grid, double GridCellSize)
+void Creature::Think(std::vector<std::vector<std::vector<Entity*> > > &grid, double GridCellSize)
 {
-
+    //Not pretty but we'll figure out a better way in the future
+    this->ProcessVisionFood(grid, GridCellSize);
+    neuron_data_.at(0) = x_coord_;
+    neuron_data_.at(1) = y_coord_;
+    neuron_data_.at(2) = orientation_;
+    neuron_data_.at(3) = energy_;
+    neuron_data_.at(4) = velocity_forward_;
+    neuron_data_.at(5) = rotational_velocity_;
+    neuron_data_.at(6) = distance_food_;
+    neuron_data_.at(7) = orientation_food_;
+    std::vector<double> output = brain_.Activate(neuron_data_);
+    velocity_forward_ = output.at(0);
+    rotational_velocity_ = output.at(1);
 }
 
-void Creature::ProcessVisionFood(std::unordered_map<int, std::unordered_map<int, std::vector<Entity*> > > &grid, double GridCellSize)
+void Creature::ProcessVisionFood(std::vector<std::vector<std::vector<Entity*> > > &grid, double GridCellSize)
 {
     Food* food = this->GetClosestFood(grid, GridCellSize);
     distance_food_ = this->GetDistance(*food);
+    orientation_food_ = this->GetRelativeOrientation(*food);
 }
 double Creature::GetMaxSize() { return max_size_; }
 void Creature::SetMaxSize(double max_size) { max_size_ = max_size; }
