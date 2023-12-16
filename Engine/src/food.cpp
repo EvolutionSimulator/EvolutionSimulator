0454c00767d01376db50146260d0593681ce6af2
ThisIsErik <erik.felvinczi@gmail.com>
2023-12-16 19:25:24 +0100

    Food and Meat fixed

--- Engine/src/food.cpp
+++ Engine/src/food.cpp
@@ -17,12 +17,13 @@ Food::Food(const double x_coord, const double y_coord)
 Food::Food(const double size)
     : Entity(size)
 {
-    //Constructor implementation
+    SetNutritionalValue(size/Environment::kMaxFoodSize * max_nutritional_value_);
 }
 
 void Food::Eat()
 {
-    SetState(Entity::Dead);
+    // Call the destructor of the base class Entity
+    this->Entity::~Entity();
 }
 
 void Food::SetNutritionalValue(double value)
@@ -35,3 +36,38 @@ double Food::GetNutritionalValue() const
     return nutritional_value_;
 }
 
+
+void Plant::Grow()
+{
+    double updated_nutritional_value = GetNutritionalValue() + photosynthesis_factor_;
+    if (updated_nutritional_value <= max_nutritional_value_)
+    {
+        SetNutritionalValue(updated_nutritional_value);
+    }
+
+    age_++;
+
+    double aging_factor = 0.02; // Adjust this factor
+    updated_nutritional_value = GetNutritionalValue() - aging_factor * age_;
+
+    if (updated_nutritional_value >= 0)
+    {
+        SetNutritionalValue(updated_nutritional_value);
+    }
+    else
+    {
+        SetState(Entity::Dead);
+    }
+}
+
+
+void Meat::Rot()
+{
+    double updated_nutritional_value = GetNutritionalValue() - rot_factor_;
+    if (updated_nutritional_value < 0)
+    {
+        SetState(Entity::Dead);
+        return;
+    }
+    SetNutritionalValue(updated_nutritional_value);
+}
