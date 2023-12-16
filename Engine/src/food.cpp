45532eed9e2b8d945cbff27716e3ef12a21b8fd2
ThisIsErik <erik.felvinczi@gmail.com>
2023-12-16 19:25:24 +0100

    Food and Meat fixed

--- Engine/src/food.cpp
+++ Engine/src/food.cpp
@@ -3,26 +3,26 @@
 #include "cstdlib"
 #include "environment.h"
 Food::Food()
-    : Entity(std::rand()%settings::environment::kMaxFoodSize), nutritional_value_(size_*settings::environment::kBaseNutritionalValue)
+    : Entity(std::rand()%settings::environment::kMaxFoodSize), nutritional_value_(size_*settings::environment::kPlantNutritionalValue)
 {
     // Constructor implementation
 }
 
 Food::Food(const double x_coord, const double y_coord)
-    : Entity(x_coord, y_coord, std::rand()%settings::environment::kMaxFoodSize), nutritional_value_(size_*settings::environment::kBaseNutritionalValue)
+    : Entity(x_coord, y_coord, std::rand()%settings::environment::kMaxFoodSize), nutritional_value_(size_*settings::environment::kPlantNutritionalValue)
 {
     //Constructor implementation
 }
 
 Food::Food(const double x_coord, const double y_coord, const double size)
-    : Entity(x_coord, y_coord, size), nutritional_value_(size_*settings::environment::kBaseNutritionalValue)
+    : Entity(x_coord, y_coord, size), nutritional_value_(size_*settings::environment::kPlantNutritionalValue)
 {
-    //Constructor implementation
 }
 
 void Food::Eat()
 {
-    SetState(Entity::Dead);
+    // Call the destructor of the base class Entity
+    this->Entity::~Entity();
 }
 
 void Food::SetNutritionalValue(double value)
@@ -35,3 +35,38 @@ double Food::GetNutritionalValue() const
     return nutritional_value_;
 }
 
+
+void Plant::Grow()
+{
+    double updated_nutritional_value = GetNutritionalValue() + settings::environment::kPhotosynthesisFactor;
+    if (updated_nutritional_value <= settings::environment::kMaxNutritionalValue)
+    {
+        SetNutritionalValue(updated_nutritional_value);
+    }
+
+    age_++;
+
+    double aging_factor = 0.02; // Adjust this factor
+    updated_nutritional_value = GetNutritionalValue() - aging_factor * age_;
+
+    if (updated_nutritional_value >= 0)
+    {
+        SetNutritionalValue(updated_nutritional_value);
+    }
+    else
+    {
+        SetState(Entity::Dead);
+    }
+}
+
+
+void Meat::Rot()
+{
+    double updated_nutritional_value = GetNutritionalValue() - settings::environment::kRotFactor;
+    if (updated_nutritional_value < 0)
+    {
+        SetState(Entity::Dead);
+        return;
+    }
+    SetNutritionalValue(updated_nutritional_value);
+}
