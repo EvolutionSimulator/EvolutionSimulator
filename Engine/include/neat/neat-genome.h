6f9689e617946366bb06635e822a3b7d80c2064f
daniela <daniela@192-168-0-190.rdsnet.ro>
2023-12-29 22:27:16 +0200

    Implement calculating the compatibility between 2 genomes
    Declare the method CompatibilityBetweenGenomes and implement it. The function
    goes throught the genomes of the two given individuals and identifies shared
    characteristics. Using these characteristics, the function calculates
    a compatibility score. There have been 3 new variables added to config.h

--- Engine/include/neat/neat-genome.h
+++ Engine/include/neat/neat-genome.h
@@ -19,45 +19,52 @@ namespace neat {
  * @details The Genome class contains neurons and links representing the
  * topology and weights of a neural network.
  */
-class Genome {
- public:
-  explicit Genome(int input_count, int output_count);
-  int GetInputCount() const;
-  int GetOutputCount() const;
-  const std::vector<Neuron>& GetNeurons() const;
-  const std::vector<Link>& GetLinks() const;
+  class Genome {
+   public:
+    explicit Genome(int input_count, int output_count);
+    int GetInputCount() const;
+    int GetOutputCount() const;
+    const std::vector<Neuron>& GetNeurons() const;
+    const std::vector<Link>& GetLinks() const;
 
-  void AddNeuron(const Neuron& neuron);
-  void AddLink(const Link& link);
+    void AddNeuron(const Neuron& neuron);
+    void AddLink(const Link& link);
 
-  void DisableNeuron(int id);  // don't use this for now
-  void DisableLink(int id);
-  void EnableNeuron(int id);  // don't use this for now
-  void EnableLink(int id);
-  void RemoveNeuron(int id);
-  void RemoveLink(int id);
+    void DisableNeuron(int id);  // don't use this for now
+    void DisableLink(int id);
+    void EnableNeuron(int id);  // don't use this for now
+    void EnableLink(int id);
+    void RemoveNeuron(int id);
+    void RemoveLink(int id);
 
-  void Mutate();
+    void Mutate();
 
-  void MutateAddNeuron();
-  void MutateAddLink();
-  void MutateRemoveNeuron();
-  void MutateRemoveLink();
-  void MutateChangeWeight();
-  void MutateChangeBias();
+    void MutateAddNeuron();
+    void MutateAddLink();
+    void MutateRemoveNeuron();
+    void MutateRemoveLink();
+    void MutateChangeWeight();
+    void MutateChangeBias();
+    void MutateActivationFunction();
 
-  bool DetectLoops(const Neuron& n);
+    bool FindNeuronById(int targetId, Neuron& foundNeuron) const;
 
-  bool HasLink(const int& in_id, const int& ou_id);
+    bool DetectLoops(const Neuron& n);
 
- private:
-  std::vector<Neuron> neurons_; /*!< A vector of Neuron objects representing the
-                                   neurons in the Genome. */
-  std::vector<Link> links_;     /*!< A vector of Link objects representing the
-                                   connections between neurons in the Genome. */
-  bool DFS(const Neuron& currentNeuron, std::unordered_set<int>& visited,
-           std::unordered_set<int>& visiting) const;
-};
+    bool HasLink(const int& in_id, const int& ou_id);
+
+    double CompatibilityBetweenGenomes(const Genome& other) const;
+
+   private:
+    std::vector<Neuron> neurons_; /*!< A vector of Neuron objects representing the
+                                     neurons in the Genome. */
+    std::vector<Link> links_;     /*!< A vector of Link objects representing the
+                                     connections between neurons in the Genome.
+                                      */
+    bool DFS(const Neuron& currentNeuron, std::unordered_set<int>& visited,
+             std::unordered_set<int>& visiting) const;
+  };
+
 
 Genome Crossover(const Genome& dominant, const Genome& recessive);
 }  // namespace neat
