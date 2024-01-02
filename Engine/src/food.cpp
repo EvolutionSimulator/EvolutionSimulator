d59d44cb10241e52cf883498fd88893000ee2c05
Ioana Popescu <117938909+ipopx@users.noreply.github.com>
2024-01-02 22:56:54 +0100

    Add acid, add tests
    Added the first implementation of the stomach acid system. Added Gtests
    for digest and bite.

--- Engine/src/food.cpp
+++ Engine/src/food.cpp
@@ -34,7 +34,7 @@ Food::Food(const double x_coord, const double y_coord, const double nutritional_
  */
 Food::Food(const double x_coord, const double y_coord, const double size, const double nutritional_value)
     : Entity(x_coord, y_coord, size),
-      nutritional_value_(size_ *nutritional_value) {}
+      nutritional_value_(size_ * nutritional_value) {}
 
 /*!
  * @brief Constructor for Food with specified size.
@@ -43,7 +43,7 @@ Food::Food(const double x_coord, const double y_coord, const double size, const 
  */
 Food::Food(const double size, const double nutritional_value)
     : Entity(size),
-    nutritional_value_(size_ *nutritional_value) {}
+    nutritional_value_(size_ * nutritional_value) {}
 
 
 /*!
