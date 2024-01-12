ae96707ef1e0394e9ab4626bfff0eda1bb56192c
berra-dogan <berra.dogan129@gmail.com>
2024-01-12 12:17:41 +0100

    addition of creature features

--- Engine/src/egg.cpp
+++ Engine/src/egg.cpp
@@ -11,18 +11,17 @@
  */
 Egg::Egg(neat::Genome genome, Mutable mutables, const double x_coord, const double y_coord)
   : Food(x_coord, y_coord, mutables.GetBabySize(), settings::environment::kEggNutritionalValue),
-  genome_(genome) {
+    Creature(genome, mutables) {
   type_ = egg;
-  mutables_ = mutables;
   age_ = 0;
   incubation_time_ = mutables.GetMaturityAge();
   fertilized_ = true; //FOR NOW WE SAY THAT THE EGG IS FERTILIZED BY DEFAULT, CHANGES IF NOT FERTILIZED UPON BEING LAYED
 }
 
 void Egg::Break(){
-    SetState(Dead);
+  Creature::SetState(Dead);
 }
 
 Creature Egg::Hatch(){
-    return Creature(genome_,mutables_);
+    return Creature(genome_,mutable_);
 }
