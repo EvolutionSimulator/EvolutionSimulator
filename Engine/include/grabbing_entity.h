527d9655c7fe49893cdd740032c1fdc2a7d602a5
GianlucaOberreit <gianluca.oberreit@polytechnique.edu>
2024-01-13 16:49:12 +0100

    Implement multi-entity acceleration logic.
    Implement linear and rotational acceleration logic for grabbing and
    grabbed bodies.

--- Engine/include/grabbing_entity.h
+++ Engine/include/grabbing_entity.h
@@ -1,41 +1,31 @@
 #ifndef GRABBING_ENTITY_H
 #define GRABBING_ENTITY_H
+#include <unordered_set>
+
 #include "movable_entity.h"
 
-class grabbing_entity : virtual public MovableEntity {
+class GrabbingEntity : virtual public MovableEntity {
  public:
-  grabbing_entity();
+  GrabbingEntity();
   double GetEffectiveAccelerationAngle() const override;
   double GetEffectiveForwardAcceleration() const override;
   double GetEffectiveRotationalAcceleration() const override;
 
-  double CalcGrabForce() const;
-  double CalcGrabForceAngle() const;
-  double CalcGrabRotForce() const;
+  std::unordered_set<MovableEntity*> GetEntities();
+  std::pair<double, double> GetCentreOfMass();
+  double GetTotalMass();
+  void UpdateEntityVelocities();
 
-  void SetGrabForce(double force);
-  void SetGrabForceAngle(double force_angle);
-  void SetGrabRotForce(double force);
-  double GetGrabForce() const;
-  double GetGrabForceAngle() const;
-  double GetGrabRotForce() const;
-
-  void SetGrabbedForce(double force);
-  void SetGrabbedForceAngle(double force_angle);
-  void SetGrabbedRotForce(double force);
-  double GetGrabbedForce() const;
-  double GetGrabbedForceAngle() const;
-  double GetGrabbedRotForce() const;
+  std::pair<double, double> GetTotalForwardAccelComps();
+  double GetTotalForwardAccel();
+  double GetTotalForwardAccelAngle();
+  double GetTotalRotAccel();
 
  private:
-  MovableEntity* grabbed_entity = nullptr;
-  // extra forces due to grabbed entity
-  double grab_force_ = 0, grab_force_angle_ = 0, grab_rot_force = 0;
-  // extra forces due to being grabbed by other entities
-  double grabbed_force_ = 0, grabbed_force_angle_ = 0, grabbed_rot_force = 0;
-  // status to check whether the entity has been affected by the grabbed_entity
-  // for the case in which 2 entities grabbed each other
-  bool affected_by_grabbed_entity = false;
+  MovableEntity* grabbed_entity_{nullptr};
+  std::unordered_set<MovableEntity*>
+      grabbing_entities_;  // entities that have grabbed this one
+  bool affected_by_grabbed_entity_{false};
 };
 
 #endif  // GRABBING_ENTITY_H
