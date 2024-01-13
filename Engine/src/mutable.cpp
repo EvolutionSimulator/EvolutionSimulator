c29473cb6266dda555c422003d68f922314f920b
Ioana Popescu <117938909+ipopx@users.noreply.github.com>
2024-01-13 23:19:36 +0100

    Fix rebase

--- Engine/src/mutable.cpp
+++ Engine/src/mutable.cpp
@@ -284,7 +284,6 @@ Mutable MutableCrossover(const Mutable &dominant, const Mutable &recessive) {
                         recessive.GetMaxSize() )/3);
   crossover.SetBabySize((2*dominant.GetBabySize() +
                          recessive.GetBabySize() )/3);
-  std::cout << (2*dominant.GetBabySize() + recessive.GetBabySize() )/3 << std::endl;
   crossover.SetMaxForce((2*dominant.GetMaxForce() +
                          recessive.GetMaxForce() )/3);
   crossover.SetGrowthFactor((2*dominant.GetGrowthFactor() +
