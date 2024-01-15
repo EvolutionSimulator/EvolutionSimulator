ce1779a22bbd36eb72bc45c31374ec22e9742620
Nikola Dobricic <nidza.dobricic@gmail.com>
2024-01-15 19:06:13 +0100

    Support different number types for graph data

--- UI/include/mainwindow/graph_manager.h
+++ UI/include/mainwindow/graph_manager.h
@@ -3,6 +3,13 @@
 
 #include <QObject>
 #include <QWidget>
+#include <QDialog>
+#include <QDebug>
+#include <QtCharts/QLineSeries>
+#include <QtCharts/QChart>
+#include <QtCharts/QChartView>
+#include <QDialog>
+#include <QVBoxLayout>
 #include "engine.h"
 
 class GraphManager : public QObject {
@@ -10,7 +17,38 @@ class GraphManager : public QObject {
 
 public:
   explicit GraphManager(QWidget* parent, Engine* engine);
-  void DrawGraph(std::vector<double> data, const QString& graphTitle);
+  template <typename T> void DrawGraph(std::vector<T> data, const QString& graphTitle) {
+
+    if (data.empty()) {
+      qDebug() << "No data to display.";
+      return;
+    }
+
+    QLineSeries* series = new QLineSeries();
+    for (size_t i = 0; i < data.size(); ++i) {
+      series->append(i, data[i]);
+    }
+
+    QChart* chart = new QChart();
+    chart->addSeries(series);
+    chart->createDefaultAxes();
+    chart->axes(Qt::Horizontal).first()->setTitleText("Time Elapsed");
+    chart->axes(Qt::Vertical).first()->setTitleText(graphTitle);
+    chart->axes(Qt::Vertical).first()->setRange(0, *std::max_element(data.begin(), data.end()) * 1.2);
+
+    QChartView* chartView = new QChartView(chart);
+    chartView->setRenderHint(QPainter::Antialiasing);
+
+    QDialog* dialog = new QDialog(parent_);
+    dialog->setWindowTitle(graphTitle);
+    QVBoxLayout* layout = new QVBoxLayout(dialog);
+    layout->addWidget(chartView);
+    dialog->setLayout(layout);
+    dialog->resize(800, 600);
+
+    connect(dialog, &QDialog::finished, dialog, &QObject::deleteLater);
+    dialog->exec();
+  }
 
 public slots:
   void DrawCreaturesOverTimeGraph();
--- UI/src/mainwindow/graph_manager.cpp
+++ UI/src/mainwindow/graph_manager.cpp
@@ -14,46 +14,6 @@ GraphManager::GraphManager(QWidget* parent, Engine *engine) :
                                                               parent_(parent),
                                                               engine_(engine){}
 
-
-
-
-void GraphManager::DrawGraph(std::vector<double> data, const QString& graphTitle) {
-
-  if (data.empty()) {
-    qDebug() << "No data to display.";
-    return;
-  }
-
-  QLineSeries* series = new QLineSeries();
-  for (size_t i = 0; i < data.size(); ++i) {
-    series->append(i, data[i]);
-  }
-
-  QChart* chart = new QChart();
-  chart->addSeries(series);
-  chart->createDefaultAxes();
-  chart->axes(Qt::Horizontal).first()->setTitleText("Time Elapsed");
-  chart->axes(Qt::Vertical).first()->setTitleText(graphTitle);
-
-  QValueAxis *axisY = qobject_cast<QValueAxis*>(chart->axes(Qt::Vertical).first());
-  if (axisY) {
-    axisY->setRange(0.0, 4.0/3.0 * axisY->max()); // Set the minimum to 0.0 and keep the current maximum
-  }
-
-  QChartView* chartView = new QChartView(chart);
-  chartView->setRenderHint(QPainter::Antialiasing);
-
-  QDialog* dialog = new QDialog(parent_);
-  dialog->setWindowTitle(graphTitle);
-  QVBoxLayout* layout = new QVBoxLayout(dialog);
-  layout->addWidget(chartView);
-  dialog->setLayout(layout);
-  dialog->resize(800, 600);
-
-  connect(dialog, &QDialog::finished, dialog, &QObject::deleteLater);
-  dialog->exec();
-}
-
 void GraphManager::DrawCreaturesOverTimeGraph() {
   auto data = engine_->GetSimulation()->GetSimulationData()->GetCreatureCountOverTime();
   DrawGraph(data, "Number of Creatures in the Simulation");
