5601c4ea36375576f95148a86b831be272f3c066
Nikola Dobricic <nidza.dobricic@gmail.com>
2024-01-21 01:22:03 +0100

    Fixed multi-thread data locking

--- Engine/include/data_accessor.h
+++ Engine/include/data_accessor.h
@@ -9,24 +9,21 @@ template <typename T> class DataAccessor {
  private:
   T &data;
   SynchronizationPrimitives &sync;
+  std::unique_lock<std::recursive_mutex> lock;
 
  public:
   DataAccessor(T &data, SynchronizationPrimitives &sync_primitives)
-      : data(data), sync(sync_primitives) {
-    std::unique_lock<std::recursive_mutex> lock(sync.mutex);
+      : data(data), sync(sync_primitives), lock(sync.mutex) {
     sync.condition_variable.wait(lock, [this] { return sync.data_ready; });
   }
 
   ~DataAccessor() {
-    sync.mutex.unlock();
-    sync.condition_variable.notify_one();
+    sync.condition_variable.notify_all();
   }
 
   T &operator*() { return data; }
 
   T *operator->() { return &data; }
-
-  void Unlock() { sync.mutex.unlock(); }
 
   static void *operator new(std::size_t) = delete;
   static void *operator new[](std::size_t) = delete;
