3c47ec11fd09c9e465f3cf2d5722899b3196437b
Milan Stankovic <milanst.soko@gmail.com>
2024-01-27 18:09:28 +0100

    Further implementation of clustering

--- UI/include/mainwindow/cluster.h
+++ UI/include/mainwindow/cluster.h
@@ -21,16 +21,25 @@ struct CreatureData {
 
 class Cluster {
  private:
-  std::unordered_map<int, CreatureData> points;
-  std::unordered_map<int, int> species;
+  std::unordered_map<int, CreatureData>
+      points;  //'keys' are creature ids, values are data points corresponding
+               // to a creature
+  std::unordered_map<int, int>
+      species;  //'keys' are creature ids, values are species labels
+  std::vector<int> core_points_ids;
   double epsilon;
   int minPts;
 
  public:
   Cluster(double epsilon, int minPts);
+
   void setPoints(const std::vector<Creature>& creatures);
   void run();
+  void add_newborns(const std::vector<Creature>& new_creatures);
+  void update_dead_creatures(const std::vector<Creature>& dead_creatures);
+
   std::unordered_map<int, int> getSpecies() const;
+  std::unordered_map<int, int> speciesSizes();
 
  private:
   std::vector<int> GetNeighbors(int id);
--- UI/src/mainwindow/cluster.cpp
+++ UI/src/mainwindow/cluster.cpp
@@ -1,4 +1,4 @@
-#include "cluster.h"
+#include "mainwindow/cluster.h"
 
 Cluster::Cluster(double epsilon, int minPts)
     : epsilon(epsilon), minPts(minPts) {}
@@ -14,7 +14,7 @@ std::vector<int> Cluster::GetNeighbors(int id) {
   std::vector<int> neighbors;
 
   for (const auto& pair : points) {
-    if (points[id].distance(pair.second) < epsilon) {
+    if (points[id].distance(pair.second) <= epsilon) {
       neighbors.push_back(pair.first);
     }
   }
@@ -25,7 +25,7 @@ std::vector<int> Cluster::GetNeighbors(int id) {
 void Cluster::expandCluster(int id, std::vector<int>& neighbors,
                             int speciesLabel) {
   species[id] = speciesLabel;
-
+  core_points_ids.push_back(id);
   for (size_t i = 0; i < neighbors.size(); ++i) {
     int neighborId = neighbors[i];
 
@@ -34,12 +34,14 @@ void Cluster::expandCluster(int id, std::vector<int>& neighbors,
         species[neighborId] =
             speciesLabel;  // Change noise point to border point
       }
-    } else {
+      // else this point already belongs to a different species, so do nothing
+    } else {  // point is not labeled
       species[neighborId] = speciesLabel;
 
       std::vector<int> newNeighbors = GetNeighbors(neighborId);
 
       if (newNeighbors.size() >= minPts) {
+        core_points_ids.push_back(neighborId);
         neighbors.insert(neighbors.end(), newNeighbors.begin(),
                          newNeighbors.end());
       }
@@ -65,3 +67,47 @@ void Cluster::run() {
 }
 
 std::unordered_map<int, int> Cluster::getSpecies() const { return species; }
+
+std::unordered_map<int, int> Cluster::speciesSizes() {
+  std::unordered_map<int, int> species_sizes;
+  for (const auto& pair : species) {
+    if (!points[pair.first].alive) {
+      continue;
+    }
+    if (species_sizes.find(pair.second) == species_sizes.end()) {
+      species_sizes[pair.second] = 1;
+    } else {
+      species_sizes[pair.second]++;
+    }
+  }
+  return species_sizes;
+}
+
+void Cluster::add_newborns(const std::vector<Creature>& new_creatures) {
+  for (const Creature& creature : new_creatures) {
+    CreatureData creature_data{creature.GetGenome(), creature.GetMutable(),
+                               true};
+    points[creature.GetID()] = creature_data;
+    // maybe core points ids should be shuffled every time
+    bool assigned_species = false;
+    for (const int& core_id : core_points_ids) {
+      if (points[core_id].distance(creature_data) <= epsilon) {
+        species[creature.GetID()] = species[core_id];
+        assigned_species = true;
+        break;
+      }
+    }
+    if (!assigned_species) {
+      species[creature.GetID()] = 0;  // noise
+    }
+  }
+}
+
+void Cluster::update_dead_creatures(
+    const std::vector<Creature>& dead_creatures) {
+  for (const Creature& creature : dead_creatures) {
+    if (points.find(creature.GetID()) != points.end()) {
+      points[creature.GetID()].alive = false;
+    }
+  }
+}
