a51979d3d37893d8058e6a30b23d2ffc755d3fea
Charlielv04 <Carloslv04@gmail.com>
2024-01-30 16:47:58 +0100

    Double gaussian

--- Engine/include/settings.h
+++ Engine/include/settings.h
@@ -133,7 +133,7 @@ class Settings {
 
   struct UISettings {
     double dragging_sensitivity = 1.0f;
-    double zoom = 3; //This really takes a toll on performance handle with care
+    double zoom = 5; //This really takes a toll on performance handle with care
     //Values of up to 5 should work but take into account it increases exponentially
     // Also UI takes the most to work in this situation so try not to render the entire map constantly
   } ui;
